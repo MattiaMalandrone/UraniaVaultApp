456f28c001de835f891b9289a90b3f6c745d87dd
Mattia Malandrone <mattia.malandrone@live.com>
2018-06-25 10:10:17 +0200

    First Commit Project

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "ns-app",
+    template: `<!-- https://docs.nativescript.org/angular/core-concepts/angular-navigation.html#page-router-outlet -->
+               <page-router-outlet></page-router-outlet>`,
+})
+
+export class AppComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+
+import { AppRoutingModule } from "./app.routing";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        NativeScriptFormsModule,
+        NativeScriptHttpClientModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    providers: [
+
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+
+export class AppModule { }
